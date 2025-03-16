ea779dc9cc205408d8911be132731972cd82aa5d
thasup <thanachonfirst@hotmail.com>
2025-03-16 16:51:40 +0700

    feat(document): Add document processing and embedding functionality
    - Introduced a new `document.py` file for handling PDF and text document loading and processing.
    - Implemented text splitting using `CharacterTextSplitter` for better document management.
    - Integrated OpenAI embeddings for querying and similarity search with Chroma vector store.
    - Updated `Pipfile` to include `chromadb` dependency for vector storage capabilities.
    - Reflected changes in `Pipfile.lock` to ensure proper dependency management.

---
+++ document.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores.chroma import Chroma
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+
+emb = embeddings.embed_query("hi there")
+
+print(emb)
+
+text_splitter = CharacterTextSplitter(
+    separator="\n",
+    chunk_size=200,
+    chunk_overlap=0
+  )
+
+loader = TextLoader("./documents/andreas_workshop.txt")
+documents = loader.load_and_split(text_splitter=text_splitter)
+
+db = Chroma.from_documents(
+  documents,
+  embedding=embeddings,
+  persist_directory="./emb",
+  collection_name="andreas_workshop"
+)
+
+results = db.similarity_search_with_score(
+  "How can I categorize things based on priority?",
+  k=4
+)
+
+for result in results:
+    print("\n")
+    print(result[1])
+    print(result[0].page_content)
